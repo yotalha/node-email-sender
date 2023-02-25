d566d2618c7310748763a95021b054504afe57e2
Talha Arshad <56502806+yotalha@users.noreply.github.com>
2023-02-26 03:05:39 +0500

    removing commented blocks

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express")
 const sgMail = require("@sendgrid/mail")
 sgMail.setApiKey("your_api_key")
-// const nodemailer = require("nodemailer")
-// const sendgridTransport = require("nodemailer-sendgrid-transport")
-
 
 const app = express()
 
@@ -27,26 +24,6 @@ app.post("/sendMail", (req, res) => {
             res.send("cannot Send Email")
     })
 })
-
-// const transporter = nodemailer.createTransport(sendgridTransport({
-//     auth: {
-//         api_key: "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"
-//     }
-// }))
-
-// app.post('/sendMail', (req, res) => {
-//     transporter.sendMail({
-//         to: "talha.work101@gmail.com",
-//         from: "test@node-mail.com",
-//         subject: "Sending mail from node",
-//         html: "<h1>Hi how are you</h1>"
-//     }).then(res => {
-//         console.log("success ", res)
-//     }).catch(err => {
-//         console.log("err ", err.message)
-//     })
-// })
-
 
 
 app.listen(3000, () => {
